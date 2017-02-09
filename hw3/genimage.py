6c2e7699a37b83fba8ac1963d3ce8973ac1d8c0d
olsenw <olsenw@onid.oregonstate.edu>
2017-02-08 19:00:01 -0800

    Python script to generate picture. See sample.png for size constraints caused by render.hs

--- hw3/genimage.py
+++ hw3/genimage.py
@@ -1,5 +1,35 @@
 from scipy import misc
+import numpy as np
+
+def printpen(state):
+    if state == True:
+        target.write("Pen Up, ")
+        return False
+    else:
+        target.write("Pen Down, ")
+        return True
+
+def printmove(x, y):
+    target.write("Move " + str(x) + " " + str(y) + ", ")
+
 arr = misc.imread("sample.png")
+np.transpose(arr, [1,0,2])
+width = arr.shape[0]
+height = arr.shape[1]
+print "image dimension: ", arr.shape
 
 target = open('output.txt', 'w')
 
+for h in range(height-1, 0, -1):
+    pen = printpen(False)
+    printmove(0, h)
+    pen = printpen(pen)
+    lsrgb = arr[0][h]
+    for w in range(0, width):
+        rgb = arr[w][h]
+        if (rgb != lsrgb).all():
+            lsrgb = rgb
+            pen = printpen(pen)
+            printmove(w, h)
+
+target.close()
