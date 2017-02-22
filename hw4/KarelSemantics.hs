6b4f9ac37c2174b66e80593be74122c9d78da34d
Evan Stefan & Will <ec2-user@ip-172-31-12-224.us-west-2.compute.internal>
2017-02-22 21:28:49 +0000

    Added IF check and case for empty Block list

--- hw4/KarelSemantics.hs
+++ hw4/KarelSemantics.hs
@@ -25,16 +25,23 @@ stmt PickBeeper _ w r = let p = getPos r
 stmt PutBeeper _ w r = let p = getPos r
                         in if (getBag r) > 0
                               then OK (incBeeper p w) (decBag r) 
-                              else Error ("No beeper to place: " ++ show p)	
+                              else Error ("No beeper to put.")	
 stmt Move _ w r      = let p = neighbor (getFacing r) (getPos r)
                         in if isClear p w
 			      then OK w (setPos p r)
 			      else Error ("Blocked at: " ++ show p)	
 stmt (Turn d) _ w r      = OK w (setFacing (cardTurn d (getFacing r) ) r)			      	
+stmt (Block []) _ w r = OK w r
+
 stmt (Block (s:xs)) d w r = case stmt s d w r of
 			OK r' w' -> stmt (Block xs) d w' r'  
 		        Error E  -> Error E
+-- If Test Stmt Stmt
+stmt (If c t e) d w r = if (test c w r) 
+			then stmt t d w r
+			else stmt e d w r
     
+stmt _ _ _ _ = Error "not def"
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
