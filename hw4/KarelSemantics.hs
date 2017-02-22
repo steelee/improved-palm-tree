1478be7317cb9ebfb25c6fe19c9fdc41493eee09
Stefan Reindel <reindels@oregonstate.edu>
2017-02-22 14:55:45 -0800

    changed Done r' to Shutdown d w r' for our case statements for iterate, while, and block statements, as per assignment definition, see Tasks part 2.

--- hw4/KarelSemantics.hs
+++ hw4/KarelSemantics.hs
@@ -39,9 +39,9 @@ stmt (Turn d) _ w r      = OK w (setFacing (cardTurn d (getFacing r) ) r)
 stmt (Block []) _ w r = OK w r
 
 stmt (Block (s:xs)) d w r = case stmt s d w r of
-			OK w' r' -> stmt (Block xs) d w' r'  
-			Done r' -> Done r'
-		        Error e  -> Error e
+			                        OK w' r' -> stmt (Block xs) d w' r'  
+			                        Done r' -> Shutdown d w r'
+		                          Error e  -> Error e
 -- If Test Stmt Stmt
 stmt (If c t e) d w r = if (test c w r) 
 			then stmt t d w r
@@ -54,14 +54,14 @@ stmt (Call m) d w r = case (lookup m d) of
 stmt (While c s) d w r = if (test c w r) 
                         then case (stmt s d w r) of 
                                 (OK w' r') -> stmt (While c s) d w' r'
-				Done r' -> Done r'
+				                        Done r' -> Shutdown d w r'
                                 Error e    -> Error e
                         else OK w r
 
 stmt (Iterate i s) d w r = if (i > 0)
                         then case (stmt s d w r) of 
                                 (OK w' r') -> stmt (Iterate (i-1) s) d w' r'
-				Done r' -> Done r'
+				                        Done r' -> Shutdown d w r'
                                 Error e -> Error e
                         else (OK w r) 
 
