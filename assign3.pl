aeda4af11f946bbdf0ea283c958fb192cb50dfeb
Stefan Reindel <reindels@oregonstate.edu>
2017-03-10 13:32:40 -0800

    provided original formatting for assignment 5.

--- assign3.pl
+++ assign3.pl
@@ -1,3 +1,6 @@
+% Here are a bunch of facts describing the Simpson's family tree.
+% Don't change them!
+
 female(mona).
 female(jackie).
 female(marge).
@@ -40,31 +43,97 @@ parent(marge,maggie).
 
 parent(selma,ling).
 
-%------------------------------------
 
+
+%%
+% Part 1. Family relations
+%%
+
+% 1. Define a predicate `child/2` that inverts the parent relationship.
 child(X,Y) :- parent(Y,X).
 
+% 2. Define two predicates `isMother/1` and `isFather/1`.
 isMother(X) :- female(X), parent(X,_). 
 isFather(X) :- male(X), parent(X,_).
 
+% 3. Define a predicate `grandparent/2`.
 grandparent(X,Y) :- parent(X,Z), parent(Z,Y).
 
+% 4. Define a predicate `sibling/2`. Siblings share at least one parent.
 sibling(X,Y) :- parent(S,X), parent(S,Y), X \= Y.
 
+% 5. Define two predicates `brother/2` and `sister/2`.
 brother(X,Y) :- male(X), sibling(X,Y).
 sister(X,Y) :- female(X), sibling(X,Y).
 
+% 6. Define a predicate `siblingInLaw/2`. A sibling-in-law is either married to
+%    a sibling or the sibling of a spouse.
 siblingInLaw(X,Y) :- sibling(Y,Z), married(X,Z).
 siblingInLaw(X,Y) :- sibling(X,Z), married(Y,Z).
 
+% 7. Define two predicates `aunt/2` and `uncle/2`. Your definitions of these
+%    predicates should include aunts and uncles by marriage.
 aunt(X,Y) :- female(X), siblingInLaw(X,Z), parent(Z,Y).
 aunt(X,Y) :- female(X), sibling(X,Z), parent(Z,Y).
 uncle(X,Y) :- male(X), sibling(X,Z), parent(Z,Y).
 uncle(X,Y) :- male(X), siblingInLaw(X,Z), parent(Z,Y).
 
+% 8. Define the predicate `cousin/2`.
 cousin(X,Y) :- child(X,S), sibling(S,T), parent(T,Y).
 
+% 9. Define the predicate `ancestor/2`.
 ancestor(X,Y) :- parent(X,Y).
 ancestor(X,Y) :- parent(Z,Y), ancestor(X,Z).
 
+% Extra credit: Define the predicate `related/2`.
 %level(X,Y) :- level((siblingInLaw(sibling(X,S),T), X \= T),Y).
+
+
+%%
+% Part 2. Language implementation
+%%
+
+% 1. Define the predicate `cmd/3`, which describes the effect of executing a
+%    command on the stack.
+
+
+% 2. Define the predicate `prog/3`, which describes the effect of executing a
+%    program on the stack.
+
+
+
+
+%------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
